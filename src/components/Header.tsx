cf7a7216970126e38e056a7b85a248584e20ef67
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2025-02-02 12:37:45 +0900

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,67 +7,14 @@ import {
 	NavigationMenuItem,
 	NavigationMenuLink,
 	NavigationMenuList,
-	navigationMenuTriggerStyle,
 } from 'src/components/ui/navigation-menu';
 import {
 	Sheet,
 	SheetContent,
-	SheetDescription,
 	SheetHeader,
 	SheetTitle,
 	SheetTrigger,
 } from 'src/components/ui/sheet';
-
-// export default function Header() {
-// 	const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-// 	return (
-// 		<header className='fixed z-50 bg-white/10 backdrop-blur-sm rounded-md start-5 end-5 top-6'>
-// 			<div className='flex items-center justify-between p-3'>
-// 				<div className='flex items-center ml-2'>
-// 					<a href='/' className='fill-red-500'>
-// 						<Icon />
-// 					</a>
-// 				</div>
-// 				<Button
-// 					className='block md:hidden'
-// 					onClick={() => setIsMenuOpen(!isMenuOpen)}
-// 				>
-// 					{/* ハンバーガーアイコンをここに追加 */}
-// 					<svg
-// 						xmlns='http://www.w3.org/2000/svg'
-// 						viewBox='0 0 24 24'
-// 						className='w-6 h-6'
-// 						fill='none'
-// 						stroke='currentColor'
-// 					>
-// 						<title>Hamburger Menu</title>
-// 						<path
-// 							strokeLinecap='round'
-// 							strokeLinejoin='round'
-// 							strokeWidth={2}
-// 							d='M4 6h16M4 12h16m-7 6h7'
-// 						/>
-// 					</svg>
-// 				</Button>
-// 				<NavigationMenu
-// 					className={`${isMenuOpen ? 'block' : 'hidden'} md:block`}
-// 				>
-// 					<NavigationMenuList>
-// 						<NavigationMenuItem>
-// 							<NavigationMenuLink
-// 								href='/'
-// 								className={`${navigationMenuTriggerStyle()} ring-1 ring-inset focus:ring-1 focus:ring-inset border-none`}
-// 							>
-// 								Home
-// 							</NavigationMenuLink>
-// 						</NavigationMenuItem>
-// 					</NavigationMenuList>
-// 				</NavigationMenu>
-// 			</div>
-// 		</header>
-// 	);
-// }
 
 export default function Header() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -75,64 +22,28 @@ export default function Header() {
 	return (
 		<header className='fixed z-50 bg-white/10 backdrop-blur-sm rounded-md start-5 end-5 top-6'>
 			<div className='flex items-center justify-between p-3'>
-				{/* <div className='flex items-center ml-2'>
-					<a href='/' className='fill-red-500'>
-                    <svg
-			xmlns='http://www.w3.org/2000/svg'
-			viewBox='0 0 576 512'
-			className='w-6 h-6'
-		>
-			<title>Home</title>
-			<HouseIcon>
-		</svg>
-					</a>
-				</div> */}
 				<a href='/'>
-					<img
-						src={astroLogo.src}
-						width='115'
-						height='48'
-						alt='Astro Homepage'
-					/>
+					<img src={astroLogo.src} className='px-2 h-10' alt='Astro Homepage' />
 				</a>
+
+				{/* Mobile Menu */}
 				<Sheet>
 					<SheetTrigger asChild>
-						{/* <Button
-							variant='outline'
-							size='icon'
-							onClick={() => setIsMenuOpen(!isMenuOpen)}
-						>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 24 24'
-								className='w-6 h-6 rounded-full'
-								fill='none'
-								stroke='currentColor'
-							>
-								<title>Hamburger Menu</title>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth={3}
-									d='M4 6h16M4 12h16m-7 6h7'
-								/>
-							</svg>
-						</Button> */}
 						<Button
 							variant='secondary'
 							size='icon'
 							onClick={() => setIsMenuOpen(!isMenuOpen)}
 							className='md:hidden'
 						>
-							<Menu size={30} strokeWidth={3} />
+							<Menu strokeWidth={3} />
 						</Button>
 					</SheetTrigger>
 					<SheetContent side='right'>
 						<SheetHeader>
 							<SheetTitle>Menu</SheetTitle>
-							<SheetDescription />
+							{/* <SheetDescription>Description</SheetDescription> */}
 						</SheetHeader>
-						<div className='flex flex-col gap-2 mt-3'>
+						<div className='flex flex-col gap-3 mt-5'>
 							<Button variant='ghost'>Home</Button>
 							<Button variant='ghost'>About</Button>
 							<Button variant='ghost'>Services</Button>
@@ -140,14 +51,28 @@ export default function Header() {
 						</div>
 					</SheetContent>
 				</Sheet>
+
+				{/* Desktop Menu */}
 				<NavigationMenu className='hidden md:block'>
 					<NavigationMenuList>
 						<NavigationMenuItem>
-							<NavigationMenuLink
-								href='/'
-								className={`${navigationMenuTriggerStyle()}`}
-							>
-								Home
+							<NavigationMenuLink asChild>
+								<Button variant='ghost'>Home</Button>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Button variant='ghost'>About</Button>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Button variant='ghost'>Services</Button>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Button variant='default'>Contact</Button>
 							</NavigationMenuLink>
 						</NavigationMenuItem>
 					</NavigationMenuList>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { ArrowRight } from 'lucide-react';
-import { Button } from 'src/components/ui/button';
 import background from 'src/assets/background.svg';
+import { Button } from 'src/components/ui/button';
 
 export default function Hero() {
 	return (
@@ -10,27 +10,21 @@ export default function Hero() {
 				backgroundImage: `url(${background.src})`,
 			}}
 		>
-			<div className='flex min-h-screen w-full bg-black/40'>
-				<div className='container flex items-center px-10'>
-					<div className='max-w-[640px]'>
-						<div className='mt-14 mb-10'>
-							<span className='text-base font-medium text-white'>
-								New Release
-							</span>
-						</div>
-						<h1 className='mb-5 text-5xl font-bold tracking-tight text-white md:text-6xl lg:text-7xl'>
-							Welcome to Our Website
-						</h1>
-						<p className='mb-10 text-xl text-gray-200'>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
-							doloremque mollitia fugiat omnis! Porro facilis quo animi
-							consequatur. Explicabo.
-						</p>
-						<Button className='gap-2 text-base' variant='secondary'>
-							Primary
-							<ArrowRight />
-						</Button>
-					</div>
+			<div className='min-h-screen w-full bg-black/40 flex items-center justify-start px-10'>
+				<div className='max-w-[640px] flex flex-col gap-5 text-white mt-20'>
+					{/* <span className='text-base font-medium text-white'>New Release</span> */}
+					<h1 className='font-bold tracking-tight  text-5xl md:text-6xl lg:text-7xl'>
+						Welcome to Website
+					</h1>
+					<p className='text-xl text-gray-200'>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Every
+						detail crafted with care! A seamless experience that inspires and
+						delights. Explore more.
+					</p>
+					<Button variant='default' className='max-w-fit'>
+						Primary
+						<ArrowRight />
+					</Button>
 				</div>
 			</div>
 		</section>
